ecac56ca6d860a0a898bb9f6274e902e5295e58d
Ashtoni95 <ashtondkirwin@gmail.com>
2024-11-19 17:03:47 -0800

    Add and Remove Methods for books

--- src/lab5/LibrarianController.java
+++ src/lab5/LibrarianController.java
@@ -16,13 +16,13 @@ public class LibrarianController {
 	public void showMembers() {
 		library.showMembers();
 	}
-	public void addBook(String title) {
+	public void addPaperBook(String title) {
 		library.addBook(new PaperBook(title));  // Book class constructor dependency
 	}
 	public void addMember(String name) {
 		library.addMember(new Member(name)); // Member class constructor dependency
 	}
-	public void removeBook(String title) {
+	public void removePaperBook(String title) {
 		library.removeBook(title); // remove 
 	}
 	public void removeMember(String name) {
@@ -36,9 +36,9 @@ public class LibrarianController {
 			System.out.println("Member " + name + " not found.");
 	}
 	public void showBook(String title) {
-		PaperBook paperBook = library.findBookByTitle(title);
-		if (paperBook != null)
-			System.out.println(paperBook);
+Book book = library.findBookByTitle(title);
+		if (book != null)
+			System.out.println(book);
 		else 
 			System.out.println("Book " + title + " not found.");
 	}
@@ -52,19 +52,37 @@ public class LibrarianController {
 	
 	public void borrowBookByMember(String title, String name) {
 		Member member = library.findMemberByName(name); // use library for search
-		PaperBook paperBook = library.findBookByTitle(title);  // use library for search
-		if (paperBook != null && member != null)
-			member.borrowBook(paperBook); // member borrows a book, not library 
+		Book book = library.findBookByTitle(title);  // use library for search
+		if (book != null && member != null)
+			member.borrowBook(book); // member borrows a book, not library 
 		else 	
 			System.out.println("Either book " + title + " or member " + name + " not found.");
 	}
 	
 	public void returnBookByMember(String title, String name) {
 		Member member = library.findMemberByName(name); // use library for search
-		PaperBook paperBook = library.findBookByTitle(title); // use library for search 
-		if (paperBook != null && member != null)
-			member.returnBook(paperBook); // members returns book. 
+		Book book = library.findBookByTitle(title); // use library for search 
+		if (book != null && member != null)
+			member.returnBook(book); // members returns book. 
 		else  	
 			System.out.println("Either book " + title + " or member " + name + " not found.");
+	}
+	public void removeEbook(String title) {
+		library.removeBook(title); // remove 
+	}
+	public void addEbook(String title) {
+		library.addBook(new Ebook(title));  // Book class constructor dependency
+	}
+	public void removeAudioBook(String title) {
+		library.removeBook(title); // remove 
+	}
+	public void addAudioBook(String title) {
+		library.addBook(new AudioBook(title));  // Book class constructor dependency
+	}
+	public void removeRareBook(String title) {
+		library.removeBook(title); // remove 
+	}
+	public void addRareBook(String title) {
+		library.addBook(new RareBook(title));  // Book class constructor dependency
 	}
 }
